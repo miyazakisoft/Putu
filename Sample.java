6a53c17d8a26dcabe14774ba2954c214af570602
宮崎光 <kou@ccgst4183.kyoto-su.ac.jp>
2019-06-24 03:10:48 +0900

    update

---
+++ Sample.java
@@ -0,0 +1,13 @@
+public class Sample{
+
+  Integer number;
+
+  public void setNumber(Integer aNumber){
+    this.number = aNumber;
+  }
+
+  public Integer getNumber(){
+    return this.number;
+  }
+
+}
