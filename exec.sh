10eb3139adf90184ff397ab64db6721335c806c0
宮崎光 <kou@ccgst5033.kyoto-su.ac.jp>
2019-12-09 12:10:35 +0900

    test

--- exec.sh
+++ exec.sh
@@ -1,6 +1,5 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 docker run --rm -v "$PWD":/home/ninerules tamada/9rules:latest A > result.txt
-cat result.txt
 #echo $SCRIPT_DIR/sample_project/$1
 #echo test
 #docker run --rm -v "$PWD":/home/ninerules tamada/9rules:latest example
