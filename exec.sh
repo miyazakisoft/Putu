907a78dab8706ab497ae24c81f334af1383ebd10
宮崎光 <kou@ccgst5033.kyoto-su.ac.jp>
2019-12-09 11:54:04 +0900

    test

--- exec.sh
+++ exec.sh
@@ -1,6 +1,7 @@
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-docker run --rm -v "$PWD":/home/ninerules tamada/9rules:latest github_project/$1 > $SCRIPT_DIR/github_project/$1/result.txt
-cat result.txt
+#SCRIPT_DIR=$(cd $(dirname $0); pwd)
+#docker run --rm -v "$PWD":/home/ninerules tamada/9rules:latest github_project/$1 > $SCRIPT_DIR/github_project/$1/result.txt
+#cat result.txt
+ls
 #echo $SCRIPT_DIR/sample_project/$1
 #echo test
 #docker run --rm -v "$PWD":/home/ninerules tamada/9rules:latest example
