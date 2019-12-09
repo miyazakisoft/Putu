f02e1f29167eafc0a963ac24aa3300b2a1337ddc
宮崎光 <kou@ccgst5033.kyoto-su.ac.jp>
2019-12-09 13:09:28 +0900

    test

--- exec.sh
+++ exec.sh
@@ -1,12 +1,12 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 docker run --rm -v "$PWD":/home/ninerules tamada/9rules:latest A > result.txt
 
-#num=$(grep line result.txt | wc -l)
+num=$(grep line result.txt | wc -l)
 
-#if [ $num -eq 0 ]; then
-#  exit 0
-#else
-#  exit 1
-#fi
+if [ $num -eq 0 ]; then
+  exit 0
+else
+  exit 1
+fi
 
-#exit 0
+exit 0
